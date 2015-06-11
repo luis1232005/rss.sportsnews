34c65757d6a1424029d6af2a77435ed6f56b8ec3
liu.liang <liu.liang@Ctrip.com>
2015-06-11 20:17:00 +0800

    add fetch.js文件

--- test/test.js
+++ test/test.js
@@ -1,7 +1,5 @@
 //加载依赖
-var fetch = require('../lib/fetch');
-var cheerio = require('cheerio');
-var parser = require('xml2json');
 var task = require('../lib/task');
 
-console.log(fetch,task);
+task.start();
+
