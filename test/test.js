985b6c72be0824796706f4cb91d565bb513e2862
liu.liang <liu.liang@Ctrip.com>
2015-06-11 19:42:33 +0800

    add fetch.js文件

--- test/test.js
+++ test/test.js
@@ -1,31 +1,7 @@
 //加载依赖
+var fetch = require('../lib/fetch');
 var cheerio = require('cheerio');
-var fs = require('fs');
-var http = require('http');
-var iconv = require('iconv-lite');
 var parser = require('xml2json');
+var task = require('../lib/task');
 
-http.get('http://rss.sina.com.cn/sports/global/england.xml',function(res){
-    var html = '';
-
-    res.setEncoding('binary');
-
-    res.on('data', function(chunk){
-        html += chunk;
-    });
-
-    res.on('end', function(){
-        html = iconv.decode(html, 'utf-8');
-        console.log(html);
-        if(!html){
-            return false;
-        }
-
-        //解析xml文档
-        var json = parser.toJson(html);
-
-        console.log(json);
-    });
-}).on('error',function(e){
-    console.log("错误：" + e.message);
-});
+console.log(fetch,task);
