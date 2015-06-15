f3b99822b4be207417cdc5dda3b9d652185b9ca0
liu.liang <liu.liang@Ctrip.com>
2015-06-15 13:45:46 +0800

    增加mongdb数据库

--- test/test.js
+++ test/test.js
@@ -1,5 +1,16 @@
 //加载依赖
 var task = require('../lib/task');
+var mongoose = require('mongoose');
+var db = mongoose.connection;
 
-task.start();
+//todo:数据库连接处理
+db.on('error', console.error.bind(console, 'connection error:'));
 
+//todo:
+db.once('open', function () {
+    //create Schema and models
+    console.log("连接成功！");
+    console.log("############################");
+    task.start();
+});
+mongoose.connect('mongodb://localhost/BnjlbDb');
--- test/test_database.js
+++ test/test_database.js
@@ -1,7 +1,24 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/BnjlbDb');
 var db = mongoose.connection;
+
+var News = require('../models/news');
+
+//todo:数据库连接处理
 db.on('error', console.error.bind(console, 'connection error:'));
+
+//todo:
 db.once('open', function (callback) {
     console.log("连接成功！")
+});
+
+//开始连接数据库
+mongoose.connect('mongodb://localhost/BnjlbDb');
+var _n = new News({
+    title : '第一条新闻'
+});
+
+_n.save(function(err, thor) {
+    if (err) return console.error(err);
+    console.log("###############");
+    console.dir(thor);
 });
