5cbbb6bbdea672d638b2e57f814c1053a1479d3b
liu.liang <liu.liang@Ctrip.com>
2015-06-18 13:47:46 +0800

    rm users.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
+//载入模块
 var express = require('express');
 var router = express.Router();
-
 var News = require('../models/News');
 
 /* GET home page. */
@@ -8,9 +8,19 @@ router.get('/', function(req, res) {
     res.render('index', { title: 'Express' });
 });
 
+//
 router.get('/getnews',function(req, res) {
-    News.findByName('test',function(err, obj){
-        res.send(obj);
+    var key = req.query.key ||'';
+    News.findByName(new RegExp('.*' + key + '.*'),function(err, obj){
+        if(err){
+            res.send({'success':false,'err':err});
+        }else{
+            //去重，排序
+            res.send({
+                'success': true,
+                'items' : obj
+            });
+        }
     });
 });
 
