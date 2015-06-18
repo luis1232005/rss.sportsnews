72e7663497e9f6f643d65d2a794f6b292296097c
liu.liang <liu.liang@Ctrip.com>
2015-06-18 12:57:22 +0800

    change gbk to utf-8

--- routes/index.js
+++ routes/index.js
@@ -1,36 +1,13 @@
-/*
- * GET home page.
- */
+var express = require('express');
+var router = express.Router();
 
-exports.index = function(req, res){
-    res.render('index', { title: 'Index' });
-};
+/* GET home page. */
+router.get('/', function(req, res) {
+    res.render('index', { title: 'Express' });
+});
 
-exports.login = function(req, res){
-    res.render('login', { title: '用户登陆'});
-};
+router.get('/getnews',function(req, res) {
+    res.render('index', { title: 'Express' });
+});
 
-exports.doLogin = function(req, res){
-    var user={
-        username:'admin',
-        password:'admin'
-    }
-
-    if(req.body.username===user.username && req.body.password===user.password){
-        req.session.user=user;
-        return res.redirect('/home');
-    } else {
-        req.session.error='用户名或密码不正确';
-        return res.redirect('/login');
-    }
-
-};
-
-exports.logout = function(req, res){
-    req.session.user=null;
-    res.redirect('/');
-};
-
-exports.home = function(req, res){
-    res.render('home', { title: 'Home'});
-};
+module.exports = router;
