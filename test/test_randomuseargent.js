272111ef460d147c59a9a4ce193cc69a15b1eb1e
liu.liang <liu.liang@Ctrip.com>
2016-05-05 15:33:56 +0800

    修改

--- test/test_randomuseargent.js
+++ test/test_randomuseargent.js
@@ -1,4 +1,78 @@
 var random_useragent = require('random-useragent');
-console.log(random_useragent.getRandom(function (ua) {
+var userAgent = random_useragent.getRandom(function (ua) {
     return ua.deviceType === 'mobile' && ua.deviceModel === "iPhone" || ua.browserName === "Android Browser";
-}));
+});
+
+console.log(userAgent);
+
+var request = require('request');
+
+var options = {
+    url: 'http://app.58.com/api/list/ershoufang?tabkey=allcity&action=getListInfo%2CgetFilterInfo%2CgetFormInfo&curVer=6.6.6.0&ct=filter&appId=1&format=json&os=android&localname=bj&v=1&geotype=baidu&location=1%2C%2C&key=&filterParams=%7B%22param1077%22%3A%220%22%2C%22filterLocal%22%3A%22bj%22%2C%22biz%22%3A%220%22%7D&geoia=%2C&params=%7B%7D',
+    headers: {
+        'User-Agent': userAgent
+    }
+};
+
+function requestPage(id,callback){
+    var arent = random_useragent.getRandom(function (ua) {
+        return ua.deviceType === 'mobile' && ua.deviceModel === "iPhone" || ua.browserName === "Android Browser";
+    });
+    var opt = {
+        url: 'http://m.58.com/bj/ershoufang/'+ id + 'x.shtml',
+        headers: {
+            'User-Agent': arent
+        }
+    };
+
+    request(opt, callback);
+}
+
+function callback(error, response, body) {
+    if (!error && response.statusCode == 200) {
+        var info = JSON.parse(body);
+        if(info && info.result && info.result.getListInfo && info.result.getListInfo.infolist){
+            console.log(info.result.getListInfo.infolist.length);
+            getPages(info.result.getListInfo.infolist);
+        }
+    }
+}
+
+request(options, callback);
+
+var result = [];
+var firstEmpty = [];
+function getPages(lists){
+    var async = require('async');
+    async.forEachOf(lists, function (item, key, callback) {
+        //fs.readFile(__dirname + value, "utf8", function (err, data) {
+        //    if (err) return callback(err);
+        //    try {
+        //        configs[key] = JSON.parse(data);
+        //    } catch (e) {
+        //        return callback(e);
+        //    }
+        //    callback();
+        //});
+        requestPage(item.infoID,function(error, response, body){
+            if (!error && response.statusCode == 200) {
+                var number = body.match(/<li\s+class=\"yellow\">[1]\d{10}<\/li>/gi);
+                if(!number){
+                   // console.log("#"+item.infoID);
+                    firstEmpty.push(item);
+                }else{
+                    number = number.join('').match(/[1]\d{10}/gi);
+                    //console.log("ok"+ number[0]);
+                    item.phonenum = number[0]||'';
+                    result.push(item);
+                }
+                callback();
+            }else{
+                return callback(error)
+            }
+        });
+    }, function (err) {
+        if (err) console.error(err.message);
+        console.log(result);
+    })
+}
